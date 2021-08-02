e53de75e9eece77e7c6b4ff7cd2c20c41c383a62
Jin <jinhong0425@gmail.com>
2021-08-01 17:05:15 -0700

    1st commit

---
+++ JsonDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("person.json"); //remember to copy always
+
+            Person person = JsonSerializer.Deserialize<Person>(text);
+
+            List<Person> people = new List<Person>() { person, person, person };
+
+            Console.WriteLine(JsonSerializer.Serialize(people));
+
+            Console.WriteLine(person.ToString());
+
+        }
+    }
+
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+
+        public Person() { }
+
+        public Person(string name, int age, char gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Person: Name:{Name}, Age:{Age}, Gender:{Gender}";
+        }
+    }
+}
